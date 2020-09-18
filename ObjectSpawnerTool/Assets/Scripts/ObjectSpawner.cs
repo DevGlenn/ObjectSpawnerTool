345e25d29b617072d97136b8b1f8427a8e13bb36
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2020-09-18 13:04:16 +0200

    Made a start of the tool

---
+++ ObjectSpawnerTool/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ObjectSpawner : EditorWindow
+{
+    private string prefabName;
+    private GameObject prefabSpawn;
+    private int prefabID = 1;
+    private Vector3 spawnLocation;
+
+    [MenuItem("Window/Prefab Spawner")]
+
+    public static void ShowWinow()
+    {
+        EditorWindow.GetWindow(typeof(ObjectSpawner));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Prefab Spawner", EditorStyles.boldLabel);
+        GUILayout.Space(10);
+
+        prefabName = EditorGUILayout.TextField("Name", prefabName);
+        prefabID = EditorGUILayout.IntField("Prefab ID", prefabID);
+
+        GUILayout.BeginHorizontal();
+        spawnLocation = EditorGUILayout.IntField(spawnLocation.x, )
+
+        GUILayout.EndHorizontal();
+
+
+        prefabSpawn = EditorGUILayout.ObjectField("Spawn a Prefab", prefabSpawn, typeof(GameObject), false) as GameObject;
+        
+        if (GUILayout.Button("Spawn A Prefab"))
+        {
+            SpawnPrefab();
+            Debug.Log("spawned an prefab");
+        }
+    }
+
+    private void SpawnPrefab()
+    {
+
+    }
+
+    
+        
+        
+}
